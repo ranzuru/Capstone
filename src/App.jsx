c385b28545b62e72ef868e480cd0d9680397cfe0
Nikuruuu <92920102+Nikuruuu@users.noreply.github.com>
2023-11-16 10:36:52 +0800

    firstz commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen text-4xl text-blue-700">
+        hello universe
+      </div>
+    </>
+  );
+}
+
+export default App;
