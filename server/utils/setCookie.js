081c6bcec6295c6174983d68fdc0e669aec110fc
Nikuruuu <92920102+Nikuruuu@users.noreply.github.com>
2024-01-02 14:57:01 +0800

    Removing the domain

--- server/utils/setCookie.js
+++ server/utils/setCookie.js
@@ -7,7 +7,6 @@ const setCookie = (res, name, value, options = {}) => {
     sameSite: isProduction ? 'None' : 'Lax',
     maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
     path: '/',
-    ...(isProduction && { domain: '.onrender.com' }), // Include the domain only in production
   };
 
   res.cookie(name, value, { ...defaultOptions, ...options });
