9ec401df8fa96f9bcb290d6c744c4bf058d3240c
Nikuruuu <92920102+Nikuruuu@users.noreply.github.com>
2024-01-02 12:20:51 +0800

    Update axios base URL

--- src/config/axios-instance.js
+++ src/config/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://sthealth-backend.onrender.com',
   withCredentials: true,
 });
 
